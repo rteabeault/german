36b2bf77e7b2cc2990617541861abd0118d8acf8
Russell Teabeault <rteabeault@gmail.com>
2012-05-12 23:18:01 -0600

    Sanitize

--- lib/german/langenscheidt/langenscheidt.rb
+++ lib/german/langenscheidt/langenscheidt.rb
@@ -8,7 +8,12 @@ class Langenscheidt
       file.readlines.collect { |line| Note.new(line) }
     end
 
-    @notes.each { |note| puts note.german }
+    output = File.join(File.dirname(csv), File.basename(csv) + ".clean.csv")
+    File.open(output, 'w') do |file|
+      @notes.each do |note| 
+        file.puts [note.sentence, note.translation, note.german, note.english].join("\t")
+      end
+    end
   end
 end
 
@@ -28,50 +33,60 @@ class Note
 
   def sanitize_german!
 
-    # reißen V/t., i., + Präp. (an, um, weg, u.a.), riss, hat (ist) gerissen
-    @german.gsub!(/\+\sPräp.\s\(.*?\)(,\s)?/, "")
-    @german.gsub!(/hat\s\(ist\)/, "hat/ist")
-    
-    if @german =~ /V\/i|V\/t|i\.,|refl\.,/
-      parts = @german.split(/\s(?:V\/|Mod|i|refl).*(?:\.,?)(?:\s\(.*\),\s)?/)
-      @german = "#{parts[0]}"
-      @german << " (#{parts[1].strip})" if parts[1]
-    end
-
-    @german.gsub!(/Pron.,?\s*/, "")
-
-    # an Präp. (+ Dat., Akk.) => an Präp.
-    # TODO ausschließlich Adv., Präp. (+ Gen. a. Dat.)
-    @german.gsub!(/\s\(\+\s.*\)/, "")
-
-    # an Präp.
     @german.chomp!("Präp.")
+    @german.sub!(/\+?\sPräp\.\s\(.*?\)(\,\s)?/, "")
     @german.chomp!(" Adj.")
     @german.chomp!(" Adv.")
     @german.chomp!(" Pron.")
     @german.chomp!(" Konj.")
 
-    # Adjectives /^(.*)\sAdj\.,?\s?(.*)/
-    if @german =~ /^(.*)\sAdj\.,?\s?(.*)/
+    @german.match(/^(.*?)\s(?=(?:refl|V\/\s?(?:t|i|I|refl)|i|Mod\.\sV)).*\.,?(.*)/) do |match|
       @german = $1
-      @german << " (#{$2})" if $2.length > 0
+      verb_parts = $2.strip
+      if verb_parts.size > 0
+        @german.concat " (#{$2.strip})"
+      end
     end
 
-    
-    if @german =~ /(.*?)\s(m|f|n),(.*)/
-      gender = "der" if $2 == "m"
-      gender = "die" if $2 == "f"
-      gender = "das" if $2 == "n"
-      @german = "#{gender} #{$1}#{$3}"
-
-      #handle feminine occupations
-      if @german =~ /(.*)\b(\p{Word}+)\b\sf,\s(.*)/
-        @german = "#{$1.chomp(", ")} / die #{$2} #{$3}"
+    @german.match(/^(.*?)\s(?=Adj|Pron|Adv).*\.,?(.*)/) do |match|
+      @german = $1
+      if $2 and $2.size > 0
+        @german.concat " (#{$2.strip})"
       end
+    end
 
-      if @german =~ /(der|die|das)\s(\w+)\s(.*)\s\/\s(die)\s(\w+)\s(.*)/
-        @german = "#{$1} #{$2} (#{$3}) / #{$4} #{$5} (#{$6})"
-      end
+    # Pilot m, -en, -en, Pilotin f, -, -nen
+    @german.match(/(.*)\s(?=m,)m,\s(.*),\s(\b\p{word}+\b)\s(?=f,)f,\s(.*)/) do |match|
+      @german = "der #{$1} (#{$2}) / die #{$3} (#{$4})"
+    end
+
+    # Zahnschmerzen nur Pl. (Zahnweh, n, -s, kein Pl.)
+    @german.match(/^(\p{word}+)\snur\sPl\.\s\((\p{word}+),\s(m|f|n),\s(.*)\)/) do |match|
+      @gender = gender_from_letter($3)
+      @german = "#{gender} #{$2} (#{$4}) [die #{$1} nur Pl.]"
+    end
+
+    # Tote m/f, -n, -n
+    @german.match(/^(\p{word}+)\sm\/f,\s(.*)/) do |match|
+      @german = "der/die #{$1} (#{$2})"
+    end
+
+    # Meter m, (auch n), -s, -
+    @german.match(/^(\p{word}+)\s(m|f|n),\s\(auch\s(m|f|n)\),(.*)/) do |match|
+      @german = "#{gender_from_letter($2)} (auch #{gender_from_letter($3)}) #{$1} (#{$4})"
+    end
+
+    # Lokomotive f, -, -n
+    @german.match(/^(\p{word}+)\s(m|f|n),\s(.*)/) do |match|
+      @german = "#{gender_from_letter($2)} #{$1} (#{$3})"
+    end
+  end
+
+  def gender_from_letter(gender_letter)
+    case gender_letter
+    when 'm' then "der"
+    when 'f' then "die"
+    when 'n' then "das"
     end
   end
 end
